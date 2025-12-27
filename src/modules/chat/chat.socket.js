452fdadb565337ff579dab4434a9746bec62bba2
sharmaprinceji <princesh1411@gmail.com>
2025-12-27 23:25:20 +0530

    chat and vedio call ready.

--- src/modules/chat/chat.socket.js
+++ src/modules/chat/chat.socket.js
@@ -1,17 +1,8 @@
 const registerChatSocket = (io) => {
     io.on("connection", (socket) => {
-        console.log("🔌 Socket connected:", socket.id);
+        console.log("Socket connected:", socket.id);
 
         // ---------------- CHAT ----------------
-        socket.on("join-room", ({ roomId, user }) => {
-            socket.join(roomId);
-            console.log(`👤 ${user} joined room ${roomId}`);
-
-            socket.to(roomId).emit("user-joined", {
-                user,
-                message: `${user} joined the chat`,
-            });
-        });
 
         socket.on("send-message", ({ roomId, user, message }) => {
             io.to(roomId).emit("receive-message", {
@@ -19,27 +10,6 @@ const registerChatSocket = (io) => {
                 message,
                 time: new Date().toISOString(),
             });
-        });
-
-        // ---------------- VIDEO ----------------
-        socket.on("join-video-room", ({ roomId }) => {
-            socket.join(roomId);
-
-            // notify existing users only
-            socket.to(roomId).emit("user-joined-video");
-        });
-
-        socket.on("offer", ({ roomId, offer }) => {
-            socket.to(roomId).emit("offer", { offer });
-        });
-
-        socket.on("answer", ({ roomId, answer }) => {
-            socket.to(roomId).emit("answer", { answer });
-        });
-
-
-        socket.on("ice-candidate", ({ roomId, candidate }) => {
-            socket.to(roomId).emit("ice-candidate", { candidate });
         });
 
         socket.on("camera-state", ({ roomId, cameraOff }) => {
@@ -50,11 +20,46 @@ const registerChatSocket = (io) => {
             });
         });
 
+       // ---------------- Vedio ----------------
+
+
+        socket.on("join-room", ({ roomId }) => {
+            socket.join(roomId);
+            console.log('56----->',roomId,socket.id)
+            socket.to(roomId).emit("user-joined", socket.id);
+        });
+
+        socket.on("offer", ({ to, offer }) => {
+            io.to(to).emit("offer", { from: socket.id, offer });
+        });
+
+        socket.on("answer", ({ to, answer }) => {
+            io.to(to).emit("answer", { from: socket.id, answer });
+        });
+
+        socket.on("ice-candidate", ({ to, candidate }) => {
+            io.to(to).emit("ice-candidate", { from: socket.id, candidate });
+        });
+
+        socket.on("media-action", (data) => {
+            socket.to(data.roomId).emit("media-action", data);
+        });
+
+        socket.on("leave-room", ({ roomId }) => {
+            console.log("remove--->",roomId, socket.id)
+            socket.leave(roomId);
+            socket.to(roomId).emit("user-left", socket.id);
+        });
+
 
         socket.on("disconnect", () => {
-            console.log("❌ Socket disconnected:", socket.id);
+            console.log("Socket disconnected:", socket.id);
         });
     });
 };
 
 export default registerChatSocket;
+
+
+
+
